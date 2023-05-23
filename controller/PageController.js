ea66bec6ce0d2d37727510842289b0c0df457009
DhanushkaTA <dhanushka999ta@gmail.com>
2023-05-23 23:23:41 +0530

    add new

---
+++ controller/PageController.js
@@ -0,0 +1,35 @@
+
+hideAll();
+$("#home").css("display","block")
+
+$("#homeBtn").on("click",function (){
+hideAll();
+$("#home").css("display","block")
+})
+
+$("#logoBtn").on("click",function (){
+    hideAll();
+    $("#home").css("display","block")
+})
+
+$("#customerBtn").on("click",function (){
+    hideAll();
+    $("#customer").css("display","block")
+})
+
+$("#itemBtn").on("click",function (){
+    hideAll();
+    $("#item").css("display","block")
+})
+
+$("#orderBtn").on("click",function (){
+    hideAll();
+    $("#order").css("display","block")
+})
+
+function hideAll(){
+  $("#home,#customer,#order,#item").css({
+      "display":"none"
+  })
+}
+
